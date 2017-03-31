2d9ea629132be1295f783fb1df58ccfe7fbdb132
Chris Hokamp <chris.hokamp@gmail.com>
2017-03-31 02:48:17 +0100

    add script to extract qe metrics from files

---
+++ scripts/qe_metrics_from_files.py
@@ -0,0 +1,56 @@
+"""
+Given a file containing hyps and a file containing gold tags, output QE metrics to file
+
+"""
+
+import codecs
+import logging
+import argparse
+import json
+
+from sequence_qe.evaluation import accuracy, f1_scores
+
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+def tags_from_file(tags_file):
+    with codecs.open(tags_file, encoding='utf8') as inp:
+        return [r.strip().split() for r in inp]
+
+
+def main(hyps_file, gold_file, output_prefix):
+    hyps = tags_from_file(hyps_file)
+    gold = tags_from_file(gold_file)
+
+    acc = accuracy(hyps, gold)
+
+    class_f1s = f1_scores(hyps, gold)
+    f1_bad = class_f1s['BAD']
+    f1_ok = class_f1s['OK']
+
+    f1_product = f1_bad * f1_ok
+
+    evaluation_record = {
+        'accuracy': acc,
+        'f1_bad': f1_bad,
+        'f1_ok': f1_ok,
+        'f1_product': f1_product
+    }
+
+    with codecs.open(output_prefix + '.json', 'w', encoding='utf8') as out:
+        out.write(json.dumps(evaluation_record))
+
+    print('ACC: {} F1_BAD: {} F1_OK: {} F1_PRODUCT: {}'.format(acc, f1_bad, f1_ok, f1_product))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--hyps", help="file containing hypotheses tags")
+    parser.add_argument("--gold", help="file containing gold tag")
+    parser.add_argument("--output", help="prefix, output will be written to `prefix.json`")
+
+    args = parser.parse_args()
+    main(args.hyps, args.gold, args.output)
