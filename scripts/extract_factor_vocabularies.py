9b6d0b80fdbbfea1abfbf769d54498fab67a0875
Chris Hokamp <chris.hokamp@gmail.com>
2017-05-04 23:28:18 +0100

    extract vocab dicts

--- scripts/extract_factor_vocabularies.py
+++ scripts/extract_factor_vocabularies.py
@@ -1,65 +1,71 @@
-'''
+"""
 Parse the concatenated src+mt factored corpus data
 
 Split out factors by the `factor_separator`, and get all tokens in the factor
 Add UNK and BOS/EOS tokens to match nematus behavior
-'''
+"""
 
-# WORKING HERE
 
-import os
-import codecs
 import logging
 import argparse
-import re
+import json
 
-import spacy
-from sequence_qe.dataset import mkdir_p
+import numpy
+
+from collections import OrderedDict
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-def extract_factors(line, nlp, factor_separator=u'|', factor_separator_replacement='-BAR-'):
-    doc = nlp(line.strip())
-    factors = [(w.orth_, w.tag_, w.dep_, w.head.tag_) for w in doc]
-    # if `factor_separator` occurs in a token, map it to factor_separator_replacement
-    factors = [tuple([re.sub(re.escape(factor_separator), factor_separator_replacement, f)
-                      for f in w_factors])
-               for w_factors in factors]
-    return factors
+def factor_iter(factor_file, num_factors, factor_separator=u'|'):
+    for row in factor_file:
+        parsed_row = [factor_tup.split(factor_separator) for factor_tup in row.split()]
+        assert all(len(f) == num_factors for f in parsed_row)
+        yield parsed_row
 
 
-def generate_spacy_factor_corpus(text_file, output_dir, lang_code, prefix, factor_separator=u'|'):
-    mkdir_p(output_dir)
-    text_output = codecs.open(os.path.join(output_dir, prefix + '.{}.'.format(lang_code) + 'tok'), 'w', encoding='utf8')
-    factor_output = codecs.open(os.path.join(output_dir, prefix + '.{}.'.format(lang_code) + 'factors'), 'w', encoding='utf8')
+def vocab_dictionaries_from_factor_iterator(factor_iterator, num_factors):
+    token_freqs = [OrderedDict() for _ in range(num_factors)]
 
-    nlp = spacy.load(lang_code)
-    logger.info('Loaded Spacy {} model'.format(lang_code))
+    for factor_row in factor_iterator:
+        for factor_tup in factor_row:
+            for idx, factor in enumerate(factor_tup):
+                if factor not in token_freqs[idx]:
+                    token_freqs[idx][factor] = 0
+                else:
+                    token_freqs[idx][factor] += 1
 
-    with codecs.open(text_file, encoding='utf8') as inp:
+    token_dicts = [OrderedDict() for _ in range(num_factors)]
 
-        for count, line in enumerate(inp):
-            row = extract_factors(line, nlp)
-            text, factors = zip(*[(factor_tup[0], factor_tup[1:]) for factor_tup in row])
-            text_output.write(u' '.join(text) + '\n')
-            factor_output.write(u' '.join([factor_separator.join(f) for f in factors]) + '\n')
-            if (count + 1) % 1000 == 0:
-                logger.info('Processed {} rows'.format(count + 1))
-            
-    logger.info('Wrote new files: {} and {}'.format(text_output.name, factor_output.name))
-    text_output.close()
-    factor_output.close()
+    for i, freq_dict in enumerate(token_freqs):
+        tokens = freq_dict.keys()
+        freqs = freq_dict.values()
+
+        sorted_idx = numpy.argsort(freqs)
+        sorted_words = [tokens[ii] for ii in sorted_idx[::-1]]
+
+        token_dicts[i]['eos'] = 0
+        token_dicts[i]['UNK'] = 1
+        for ii, ww in enumerate(sorted_words):
+            token_dicts[i][ww] = ii+2
+
+    return token_dicts
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", help="Input file with raw text")
+    parser.add_argument("-i", "--input", help="Input file with factor tuples separated by `factor_separator`")
     parser.add_argument("-o", "--output", help="Directory where output files will be written")
-    parser.add_argument("-l", "--lang", help="Two-character language code used to load the correct Spacy model")
-    parser.add_argument("-p", "--prefix", help="prefix for output files")
+    parser.add_argument("-n", "--num_factors", help="the number of factors")
 
     args = parser.parse_args()
-    generate_spacy_factor_corpus(args.input, args.output, args.lang, args.prefix)
+
+    factor_iterator = factor_iter(open(args.input, 'r'), args.num_factors)
+    factor_dicts = vocab_dictionaries_from_factor_iterator(factor_iterator, num_factors=args.num_factors)
+
+    for idx, filename in enumerate(['factor_{}'.format(i+1) for i in range(args.num_factors)]):
+        with open('%s.json'%filename, 'wb') as f:
+            json.dump(factor_dicts[idx], f, indent=2, ensure_ascii=False)
+        logger.info('Wrote index to: {}'.format(filename))
