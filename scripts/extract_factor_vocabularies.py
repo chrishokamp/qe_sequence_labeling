fa0104c2b1c472ff0a046abab0b4121e02f14ba0
Chris Hokamp <chris.hokamp@gmail.com>
2017-05-04 23:55:24 +0100

    bugfixes in feature dict extraction

--- scripts/extract_factor_vocabularies.py
+++ scripts/extract_factor_vocabularies.py
@@ -9,10 +9,13 @@ Add UNK and BOS/EOS tokens to match nematus behavior
 import logging
 import argparse
 import json
+import os
 
 import numpy
 
 from collections import OrderedDict
+
+from sequence_qe.dataset import mkdir_p
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
@@ -29,13 +32,15 @@ def factor_iter(factor_file, num_factors, factor_separator=u'|'):
 def vocab_dictionaries_from_factor_iterator(factor_iterator, num_factors):
     token_freqs = [OrderedDict() for _ in range(num_factors)]
 
-    for factor_row in factor_iterator:
+    for count, factor_row in enumerate(factor_iterator):
         for factor_tup in factor_row:
             for idx, factor in enumerate(factor_tup):
                 if factor not in token_freqs[idx]:
                     token_freqs[idx][factor] = 0
                 else:
                     token_freqs[idx][factor] += 1
+        if (count + 1) % 10000 == 0:
+            logger.info('Processed {} rows'.format(count + 1))
 
     token_dicts = [OrderedDict() for _ in range(num_factors)]
 
@@ -58,14 +63,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", help="Input file with factor tuples separated by `factor_separator`")
     parser.add_argument("-o", "--output", help="Directory where output files will be written")
-    parser.add_argument("-n", "--num_factors", help="the number of factors")
+    parser.add_argument("-n", "--num_factors", type=int, help="the number of factors")
 
     args = parser.parse_args()
 
     factor_iterator = factor_iter(open(args.input, 'r'), args.num_factors)
     factor_dicts = vocab_dictionaries_from_factor_iterator(factor_iterator, num_factors=args.num_factors)
+    mkdir_p(args.output)
 
     for idx, filename in enumerate(['factor_{}'.format(i+1) for i in range(args.num_factors)]):
-        with open('%s.json'%filename, 'wb') as f:
+        with open('%s.json'%os.path.join(args.output, filename), 'wb') as f:
             json.dump(factor_dicts[idx], f, indent=2, ensure_ascii=False)
         logger.info('Wrote index to: {}'.format(filename))
