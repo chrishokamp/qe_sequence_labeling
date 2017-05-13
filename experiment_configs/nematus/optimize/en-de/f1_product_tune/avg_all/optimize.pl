c5b8b5293239ac1793426f32d85ce10df64a0b10
Chris Hokamp <chris.hokamp@gmail.com>
2017-05-13 13:22:14 +0100

    added script to optimize avg_all ensemble for f1_product

--- experiment_configs/nematus/optimize/en-de/f1_product_tune/avg_all/optimize.pl
+++ experiment_configs/nematus/optimize/en-de/f1_product_tune/avg_all/optimize.pl
@@ -22,12 +22,16 @@ my $CONCAT_FACTORS_MODEL_DIR = "/media/1tb_drive/nematus_ape_experiments/ape_qe/
 
 my $DEV_DATA_DIR = "/media/1tb_drive/Dropbox/data/qe/ape/concat_wmt_2016_2017";
 
+# Note: reference (post-edited) translation is never used in the QE evaluation, because we already have the tags
+#my $DEV_REF = "$DEV_DATA_DIR/dev.pe";
+my $DEV_MT = "$DEV_DATA_DIR/dev.mt";
+my $DEV_TAGS = "$DEV_DATA_DIR/dev.tags";
 my $DEV_REF = "$DEV_DATA_DIR/dev.pe";
 
 my $time = time();
 my $WORK = "tuning.$time";
 
-my $SCORER = "TER";
+my $SCORER = "QE";
 
 my $MAX_IT = 10;
 
@@ -38,7 +42,6 @@ GetOptions(
     "i|maximum-iterations=i" => \$MAX_IT,
 );
 
-#my $MIRA = "$MOSES_DIR/kbmira";
 my $MERT = "$MOSES_DIR/mert";
 my $EVAL = "$MOSES_DIR/evaluator";
 my $EXTR = "$MOSES_DIR/extractor";
@@ -56,7 +59,8 @@ execute("mkdir -p $WORK");
 
 # start with a default run1.dense (don't call the `amunmt show weights`)
 # Note filename must be run1.dense
-my $START_WEIGHTS = "/home/chris/projects/qe_sequence_labeling/experiment_configs/nematus/optimize/en-de/avg_all/run1.dense";
+# Note this copies the weights specific to this combination of models
+my $START_WEIGHTS = "/home/chris/projects/qe_sequence_labeling/experiment_configs/nematus/optimize/en-de/src-mt-mt+src/run1.dense";
 execute("cp $START_WEIGHTS $WORK");
 
 # uses default run1.dense to create run1.initopt
@@ -67,27 +71,37 @@ for my $i (1 .. $MAX_IT) {
     unless(-s "$WORK/run$i.out") {
         execute("python $GBS_DIR/scripts/translate_nematus.py -m $SRC_MODEL_DIR/model.4-best.averaged.npz $MT_MODEL_DIR/model.4-best.averaged.npz $MT_ALIGN_MODEL_DIR/model.4-best.averaged.npz $CONCAT_MODEL_DIR/model.4-best.averaged.npz $CONCAT_FACTORS_MODEL_DIR/model.4-best.averaged.npz -c $SRC_MODEL_DIR/model.npz.json $MT_MODEL_DIR/model.npz.json $MT_ALIGN_MODEL_DIR/model.npz.json $CONCAT_MODEL_DIR/model.npz.json $CONCAT_FACTORS_MODEL_DIR/model.npz.json -i $DEV_DATA_DIR/dev.src.prepped $DEV_DATA_DIR/dev.mt.prepped $DEV_DATA_DIR/dev.mt.factor_corpus $DEV_DATA_DIR/dev.src-mt.concatenated $DEV_DATA_DIR/spacy_factor_corpus/dev.src-mt.concatenated.bpe.factor_corpus --nbest $NBEST --beam_size $BEAM_SIZE --length_factor $LENGTH_FACTOR --load_weights $WORK/run$i.dense --mert_nbest | sed 's/\@\@ //g' | $MOSES_SCRIPTS/recaser/detruecase.perl | $MOSES_SCRIPTS/tokenizer/deescape-special-chars.perl > $WORK/run$i.out");
     }
-    execute("$EVAL $CONFIG --reference $DEV_REF -n $WORK/run$i.out | tee -a $WORK/progress.txt");
+    execute("$EVAL $CONFIG --reference $DEV_MT -n $WORK/run$i.out | tee -a $WORK/progress.txt");
 
     my $j = $i + 1;
     unless(-s "$WORK/run$j.dense") {
-        execute("$EXTR $CONFIG --reference $DEV_REF -n $WORK/run$i.out -S $WORK/run$i.scores.dat -F $WORK/run$i.features.dat");
+        execute("$EXTR $CONFIG --reference $DEV_MT -n $WORK/run$i.out -S $WORK/run$i.scores.dat -F $WORK/run$i.features.dat");
+
+        # hacky hacks - start
+        # Chris: note *5 suffix -- this is nbest list len
+        execute("cat $DEV_MT | perl -ne 'print \$_ x 5' > $WORK/dev.mt.5");
+        execute("cat $DEV_TAGS | perl -ne 'print \$_ x 5' > $WORK/dev.mt.tags.5");
+        # Chris: removed moses postprocessing from original script because we do this in the translation command above
+#        execute("cat $WORK/run$i.out | perl -ne 'chomp; \@t = split(/\\|\\|\\|/, \$_); print \"\$t[1]\\n\"' | perl $SCRIPTS/detruecase.perl | $SCRIPTS/deescape-special-chars.perl > $WORK/run$i.out.12");
+        execute("cat $WORK/run$i.out | perl -ne 'chomp; \@t = split(/\\|\\|\\|/, \$_); print \"\$t[1]\\n\"' > $WORK/run$i.out.5");
+
+        execute("python run_tercom.py -target_file=$WORK/dev.mt.5 -post_edited_file=$WORK/run$i.out.5 2>/dev/null >/dev/null");
+        execute("python parse_pra.py -tercom_file=$WORK/run$i.out.5.tercom.out.pra");
+        execute("python generate_wmt_submission.py DCU $WORK/dev.mt.5 $WORK/run$i.out.5.tercom.out.pra.tags > $WORK/run$i.out.5.qe.submission");
+        execute("python evaluate_wmt15_stats.py $WORK/dev.mt.5 $WORK/dev.mt.tags.5 $WORK/run$i.out.5.qe.submission | perl toScores.pl > $WORK/run$i.scores.dat");
+        execute("rm $WORK/run$i.out.5.tercom.*");
+        # hacky hacks - end
 
         my $SCORES = join(",", map { "$WORK/run$_.scores.dat" } (1 .. $i));
         my $FEATURES = join(",", map { "$WORK/run$_.features.dat" } (1 .. $i));
 
-        # Note: the -d argument is the number of dimensions == the number of features
+        # Chris: the -d parameter below corresponds to the number of features in this optimization run
         execute("$MERT --sctype $SCORER --scfile $SCORES --ffile $FEATURES --ifile $WORK/run$i.initopt -d 5 -n 20 -m 20 --threads 20 2> $WORK/mert.run$i.log");
 
         log2dense("$WORK/mert.run$i.log", "$WORK/run$j.dense");
         dense2init("$WORK/run$j.dense", "$WORK/run$j.initopt");
     }
     execute("cp $WORK/run$j.dense $WORK/weights.txt")
-
-    # WORKING: run eval script and print metrics
-    # WORKING: extract nbest list from MERT output
-    # WORKING: proper postprocessing on n-best list
-#    execute("cat run$j.out | perl -ne 'chomp; @t = split(/\\|\\|\\|/, $_); print \"$t[1]\n\"' | sed -n '1~5p' > run1.1best.out")
 }
 
 sub execute {
