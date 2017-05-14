12b660a8dcada1b5d252b8e4929990998ab0cc46
Chris Hokamp <chris.hokamp@gmail.com>
2017-05-14 02:02:03 +0100

    fix nbest param in toScores.pl

--- experiment_configs/nematus/optimize/en-de/f1_product_tune/resources/toScores.pl
+++ experiment_configs/nematus/optimize/en-de/f1_product_tune/resources/toScores.pl
@@ -1,12 +1,12 @@
 use strict;
 
 my $c = 0;
-print "SCORES_TXT_BEGIN_0 $c 12 4 QE\n";
+print "SCORES_TXT_BEGIN_0 $c 5 4 QE\n";
 while(<STDIN>) {
-  if(($. - 1)  % 12 == 0 and $. > 1) {
+  if(($. - 1)  % 5 == 0 and $. > 1) {
      $c++;
      print "SCORES_TXT_END_0\n";
-     print "SCORES_TXT_BEGIN_0 $c 12 4 QE\n";
+     print "SCORES_TXT_BEGIN_0 $c 5 4 QE\n";
   }
   print $_;
 }
